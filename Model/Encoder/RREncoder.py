7ee581da0ead8e978ef57b3429a7efe838b65a4f
Yasantha Niroshan <niroshanyi@gmail.com>
2026-02-08 08:16:08 +0530

    Added Minor Update - Updated Encoder with Group and Layer Normalization - Removed the Global Average Pooling
    Need to add SE block

--- Model/Encoder/RREncoder.py
+++ Model/Encoder/RREncoder.py
@@ -6,13 +6,21 @@ class Encoder(nn.Module):
     def __init__(self, latent_dim=LATENT_SIZE):
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Conv1d(1, 16, kernel_size=5, padding=2),
+            nn.Conv1d(1, 8, kernel_size=5, padding=2,stride=2), # [B, 1, 50] -> [B, 8, 25]
+            nn.GroupNorm(2,8),
             nn.ReLU(),
-            nn.Conv1d(16, 32, kernel_size=5, padding=2),
+            nn.Conv1d(8, 16, kernel_size=5, padding=2,stride=2), # [B, 8, 25] -> [B, 16, 13]
+            nn.GroupNorm(4,16),
             nn.ReLU(),
-            nn.AdaptiveAvgPool1d(1)
+            nn.Conv1d(16, 32, kernel_size=5, padding=2,stride=2), # [B, 16, 13] -> [B, 32, 7]
+            nn.GroupNorm(8,32),
+            nn.ReLU(),
         )
-        self.proj = nn.Linear(32, latent_dim)
+        self.proj = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(32 * 7, latent_dim),
+            nn.LayerNorm(latent_dim)
+            )
 
     def forward(self, rr_window)->torch.Tensor:
         """
@@ -20,6 +28,6 @@ class Encoder(nn.Module):
         output:    [B, LATENT_SIZE]
         """
         x = rr_window.unsqueeze(1)
-        h = self.encoder(x).squeeze(-1)
+        h = self.encoder(x)  # [B, 32, 7]
         z = self.proj(h)
         return z
